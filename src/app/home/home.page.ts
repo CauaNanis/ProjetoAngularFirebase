9821de4e7ab9e5e09dec8189bab75a90114d598b
Ligerinho <caua.nanis.rosa2009@gmail.com>
2026-04-22 21:20:13 +0000

    pagina final

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,11 +7,68 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
 
-  
+  nome: string = '';
+  email: string = '';
+  senha: string = '';
+  confirmarSenha: string = '';
+
+  constructor() {}
+
+  criarConta() {
+
+  if (this.nome == '') {
+    alert('Digite o nome');
+  }
+
+  if (this.email == '') {
+    alert('Digite o e-mail');
+  }
+
+  if (!this.email.includes('@') || !this.email.includes('.')) {
+    alert('Digite um e-mail válido');
+    return;
+  }
+
+  if (this.senha == '') {
+    alert('Digite a senha');
+  }
+
+  if (this.confirmarSenha == '') {
+    alert('Confirme a senha');
+  }
+
+  if (this.senha != '' && this.confirmarSenha != '' && this.senha != this.confirmarSenha) {
+    alert('As senhas estão diferentes');
+  }
+
+  if (
+    this.nome != '' &&
+    this.email != '' &&
+    this.email.indexOf('@') != -1 &&
+    this.senha != '' &&
+    this.confirmarSenha != '' &&
+    this.senha == this.confirmarSenha
+  ) {
+    alert('Conta criada com sucesso');
+  }
+
+}
+
+  facebook() {
+    alert('Login com Facebook');
+  }
+
+  twitter() {
+    alert('Login com Twitter');
+  }
+
+  google() {
+    alert('Login com Google');
+  }
 
 }
